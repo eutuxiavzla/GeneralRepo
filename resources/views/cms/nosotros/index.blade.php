78fd172c1b42b4d58618834b186f15476ab61628
AndresG <gazuiandres@gmail.com>
2020-08-31 15:27:50 -0400

    mini cambio

--- resources/views/cms/nosotros/index.blade.php
+++ resources/views/cms/nosotros/index.blade.php
@@ -69,7 +69,7 @@
 	  <div class="modal-dialog">
 	    <div class="modal-content">
 	      <div class="modal-header">
-	        <h5 class="modal-title" id="exampleModalLabel">Eliminar Banner</h5>
+	        <h5 class="modal-title" id="exampleModalLabel">Eliminar Sección</h5>
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 	          <span aria-hidden="true">&times;</span>
 	        </button>
